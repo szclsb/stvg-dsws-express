7fb6b840a3cbb75ffa8889f7c471750f9249f4d5
Claudio Seitz <szclsb@gmail.com>
2023-01-15 13:49:18 +0100

    Impl disciplines and fixed some validations

--- src/models/athlete.ts
+++ src/models/athlete.ts
@@ -1,4 +1,4 @@
-import {validate, validateIn, validateName, validateInteger, required} from "../utils/validation-utils";
+import {validate, validateIn, validateString, validateInteger, required, validateText} from "../utils/validation-utils";
 
 export interface Athlete {
     firstName: string;
@@ -7,13 +7,13 @@ export interface Athlete {
     yearOfBirth: number;
 }
 
-const sexes = ["FEMALE", "MALE"] as const;
+export const sexes = ["FEMALE", "MALE"] as const;
 export type Sex = typeof sexes[number];
 
 export async function validateAthlete(body: any): Promise<Athlete> {
     return await validate<Athlete>({
-        firstName: validateName(body.firstName).then(required),
-        lastName: validateName(body.lastName).then(required),
+        firstName: validateText(body.firstName).then(required),
+        lastName: validateText(body.lastName).then(required),
         sex: validateIn<Sex>(sexes, body.sex).then(required),
         yearOfBirth: validateInteger(body.yearOfBirth, 1900, 2023).then(required)
     });
---
+++ src/routes/discipline-route.ts
@@ -0,0 +1,50 @@
+import {Db, WithId} from "mongodb";
+import {ObjectID} from "bson";
+import {validateDiscipline} from "../models/discipline";
+import {errorCallback} from "../utils/route-utils"
+import express, {Request, Router} from "express";
+
+export const path = '/api/v1/disciplines';
+
+export function initDisciplineRoute(db: Db): Router {
+    const router = express.Router();
+    const collection = db.collection('disciplines');
+
+    router.post("/", (req, res) => {
+        validateDiscipline(req.body).then(discipline => collection.insertOne(discipline).then(insertedId => {
+            res.setHeader('Location', `${path}/${insertedId}`).status(201).send();
+        })).catch(errorCallback(res));
+    });
+    router.get("/", (req, res) => {
+        collection.aggregate([]).toArray().then((doc: WithId<Document>[]) => {
+            res.status(200).json(doc)
+        }).catch(errorCallback(res));
+    });
+    router.get("/:id", (req, res) => {
+        collection.findOne({
+            _id: ObjectID.createFromHexString(req.params.id as string)
+        }).then((doc: WithId<Document>) => {
+            res.status(200).json(doc)
+        }).catch(errorCallback(res));
+    });
+    router.put("/:id", (req, res) => {
+        validateDiscipline(req.body).then(discipline => {
+            collection.findOneAndUpdate({
+                _id: ObjectID.createFromHexString(req.params.id as string)
+            }, {
+                $set: discipline
+            }).then(() => {
+                res.status(204).send();
+            })
+        }).catch(errorCallback(res));
+    });
+    router.delete("/:id", (req, res) => {
+        collection.findOneAndDelete({
+            _id: ObjectID.createFromHexString(req.params.id as string)
+        }).then(() => {
+            res.status(204).send();
+        }).catch(errorCallback(res));
+    });
+    console.debug(`initialized route ${path}`);
+    return router;
+}
