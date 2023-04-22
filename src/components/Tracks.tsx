0e7235cd2c8c7c2655b1ac9ab86eb82bd6247297
Claudio Seitz <szclsb@gmail.com>
2023-04-22 12:07:20 +0200

    small ui planning fixes

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -13,12 +13,16 @@ interface TrackProperties {
     plannings: [Planning, Athlete[], string?][];
 }
 
-function em(value: number) {
+function em(value: number): string {
     return value + "em";
 }
 
-function seq(count: number) {
+function seq(count: number): number[] {
     return new Array(count).fill(1).map((_, i) => i + 1)
+}
+
+function pad(value: number, digits: number): string {
+    return String(value).padStart(digits, '0')
 }
 
 function Tracks(props: TrackProperties) {
@@ -31,24 +35,25 @@ function Tracks(props: TrackProperties) {
             minute: startTime.minute + i * sepTime.minute,
         }
     })
-    const width = props.leftHeaderWidth + timesCount * (props.separator + props.itemWidth);
-    const height = props.topHeaderHeight + props.separator + timesCount * (props.separator + props.itemHeight);
+    const width = props.leftHeaderWidth + timesCount * (props.itemWidth);
+    const height = props.topHeaderHeight + props.tracks * (props.itemHeight);
     return (
         <div style={{
             position: "relative",
             width: em(width),
-            height: em(height)
+            height: em(height + props.separator / 2)
         }}>
             {timesSeq.map((t, i) => <div style={{
                 position: "absolute",
                 top: em(0),
-                left: em(props.leftHeaderWidth + (props.separator + props.itemWidth) * i),
+                left: em(props.leftHeaderWidth - 2 + (props.itemWidth * i)),
                 height: em(props.topHeaderHeight),
-                width: em(props.itemWidth)
-            }}>{t.hour}:{t.minute}</div>)}
+                textAlign: "center",
+                width: "4em"
+            }}>{pad(t.hour, 2)}:{pad(t.minute, 2)}</div>)}
             {seq(props.tracks).map(i => <div style={{
                 position: "absolute",
-                top: em(props.topHeaderHeight + (props.separator + props.itemHeight) * (i - 1)),
+                top: em(props.topHeaderHeight + (props.itemHeight) * (i - 1)),
                 left: em(0),
                 height: em(props.itemHeight),
                 width: em(props.leftHeaderWidth),
@@ -56,10 +61,18 @@ function Tracks(props: TrackProperties) {
             }}>Bahn {i}</div>)}
             {seq(props.tracks + 1).map(i => <div style={{
                 position: "absolute",
-                top: em(props.topHeaderHeight + (props.separator + props.itemHeight) * (i - 1)),
+                top: em(props.topHeaderHeight - props.separator / 2 + (props.itemHeight) * (i - 1)),
                 left: em(0),
                 width: em(width),
                 height: em(props.separator),
+                backgroundColor: "black"
+            }}/>)}
+            {timesSeq.map((_, i) => <div style={{
+                position: "absolute",
+                top: em(props.topHeaderHeight - 0.5),
+                left: em( props.leftHeaderWidth - props.separator / 2 + (props.itemWidth) * i),
+                width: em(props.separator),
+                height: em(height - props.topHeaderHeight + 0.5),
                 backgroundColor: "black"
             }}/>)}
         </div>
