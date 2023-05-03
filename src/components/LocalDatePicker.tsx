0aa54d79382197b305f66d00265ad1d645dbd428
Claudio Seitz <szclsb@gmail.com>
2023-05-03 21:29:42 +0200

    prepared planning

--- src/components/LocalDatePicker.tsx
+++ src/components/LocalDatePicker.tsx
@@ -1,11 +1,10 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider"
 import {AdapterMoment} from "@mui/x-date-pickers/AdapterMoment"
 import {DatePicker} from "@mui/x-date-pickers/DatePicker"
-import React from "react";
+import {LocalDate} from "../models/models";
 import moment, {Moment} from "moment";
 import "moment/locale/de-ch"
-import {LocalDate} from "../models/models";
 
 function convert(localDate?: LocalDate): Moment | undefined {
     return !localDate ? undefined : moment(new Date(localDate.year, localDate.month - 1, localDate.day));
