3951ebfb51c17e475fe300acc395423afe856592
Claudio Seitz <szclsb@gmail.com>
2023-05-04 22:14:50 +0200

    implemented login notification

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, Stack, TextField} from "@mui/material";
+import {Alert, AlertColor, Box, Button, Snackbar, Stack, TextField} from "@mui/material";
 import React, {ChangeEvent, useState} from "react";
 import {Client, Method} from "../client";
 import {Buffer} from 'buffer'
@@ -8,6 +8,13 @@ const client = new Client("/api/login");
 export function LoginView() {
     const [account, setAccount] = useState<string>();
     const [password, setPassword] = useState<string>();
+    const [notification, setNotification] = useState<{
+        show: boolean,
+        message?: string,
+        severity?: AlertColor
+    }>({
+        show: false
+    });
 
     const onAccountChange = (e: ChangeEvent<HTMLInputElement>) => setAccount(e.target.value);
     const onPasswordChange = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
@@ -20,8 +27,24 @@ export function LoginView() {
             }
         }).then(data => {
             localStorage.setItem('api-key', data.apiKey);
-        }).catch(err => console.error(err));
+            setNotification({
+                show: true,
+                message: "Erfolgreich eingeloggt",
+                severity: "success"
+            });
+        }).catch(err => {
+            console.error(err);
+            setNotification({
+                show: true,
+                message: `Login fehlgeschlagen: ${err.message}`,
+                severity: "success"
+            });
+        });
     }
+
+    const onNotificationClose = () => setNotification({
+        show: false
+    });
 
     return (<Box alignContent="center" sx={{ m: 2 }}>
         <Stack spacing={2} maxWidth={0.5}>
@@ -29,5 +52,10 @@ export function LoginView() {
             <TextField label="Passwort" type="password" value={password} onChange={onPasswordChange} />
             <Button onClick={login}>Anmelden</Button>
         </Stack>
+        <Snackbar open={notification.show} autoHideDuration={5000} onClose={onNotificationClose}>
+        <Alert severity={notification.severity} sx={{width: '100%'}}>
+            {notification.message}
+        </Alert>
+    </Snackbar>
     </Box>);
 }
