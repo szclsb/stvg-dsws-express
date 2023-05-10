769cc050f32cfd6b400f6b923886b82562395350
Claudio Seitz <szclsb@gmail.com>
2023-05-10 18:19:24 +0200

    exposed location header through cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -20,7 +20,9 @@ const datasource = new Datasource();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    exposedHeaders: 'Location'
+}));
 // app.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Api-Key");
